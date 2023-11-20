b6bbbb76dc35e6a05b23e0b347af5f396cd1ecde
gumanzanoo <gustavomazanoluiz@hotmail.com>
2023-11-20 20:24:05 -0300

    chore

--- app/src/main/java/unipar/invictus/app/config/Response.java
+++ app/src/main/java/unipar/invictus/app/config/Response.java
@@ -1,21 +1,17 @@
 package unipar.invictus.app.config;
 
 public class Response {
-
-    public static final int OK = 200;
-    public static final int CREATED = 201;
-    public static final int BAD_REQUEST = 400;
-    public static final int NOT_FOUND = 404;
-    public static final int INTERNAL_SERVER_ERROR = 500;
-    public int status;
+    public static final String SUCCESS = "success";
+    public static final String ERROR = "error";
+    public String status;
     public String message;
 
-    public Response(int status, String message) {
+    public Response(String status, String message) {
         this.status = status;
         this.message = message;
     }
 
-    public static Response response(int status, String message) {
+    public static Response response(String status, String message) {
         return new Response(status, message);
     }
 }
