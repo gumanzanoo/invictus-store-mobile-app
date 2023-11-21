45c8ea1d767d8763956791143bf73f353bc013e1
gumanzanoo <gustavomanzanoluiz@hotmail.com>
2023-11-21 15:57:07 -0300

    chore

---
+++ app/src/main/java/unipar/invictus/app/controller/ClienteController.java
@@ -0,0 +1,86 @@
+package unipar.invictus.app.controller;
+
+import unipar.invictus.app.helpers.Response;
+import unipar.invictus.app.entity.Cliente;
+import unipar.invictus.app.repository.ClienteRepository;
+
+public class ClienteController {
+    public Response getById(int id) {
+        return Response.response(Response.SUCCESS, clienteRepository.getById(id).toString());
+    }
+
+    public Response getAll() {
+        return Response.response(Response.SUCCESS, clienteRepository.getAll().toString());
+    }
+
+    public Response create(String nome, String documento, String email) {
+        Cliente savedCliente = clienteRepository.getByEmail(email);
+
+        if (savedCliente.getDocumento().equals(documento)) {
+            return Response.response(Response.ERROR,
+                    "Já existe um cliente cadastrado com este Documento");
+        }
+
+        if (savedCliente.getEmail().equals(email)) {
+            return Response.response(Response.ERROR,
+                    "Já existe um cliente cadastrado com este e-mail");
+        }
+
+        Cliente cliente = new Cliente();
+        cliente.setNome(nome);
+        cliente.setDocumento(documento);
+        cliente.setEmail(email);
+
+        long clienteRegistered = clienteRepository.insert(cliente);
+
+        if (clienteRegistered > 0) {
+            return Response.response(Response.SUCCESS,
+                    "Cliente cadastrado com sucesso");
+        }
+
+        return Response.response(Response.ERROR,
+                "Não foi possível cadastrar o cliente");
+    }
+
+    public Response update(int id, String nome, String documento, String email) {
+        Cliente cliente = clienteRepository.getById(id);
+
+        if (cliente == null) {
+            return Response.response(Response.ERROR,
+                    "Este cliente não está cadastrado.");
+        }
+
+        cliente.setNome(nome);
+        cliente.setDocumento(documento);
+        cliente.setEmail(email);
+
+        int clienteUpdated = clienteRepository.update(cliente);
+
+        if (clienteUpdated > 0) {
+            return Response.response(Response.SUCCESS,
+                    "Cliente atualizado com sucesso");
+        }
+
+        return Response.response(Response.ERROR,
+                "Não foi possível atualizar o cliente");
+    }
+
+    public Response delete(int id) {
+        Cliente cliente = clienteRepository.getById(id);
+
+        if (cliente == null) {
+            return Response.response(Response.ERROR,
+                    "Este cliente não está cadastrado.");
+        }
+
+        int clienteDeleted = clienteRepository.delete(cliente);
+
+        if (clienteDeleted > 0) {
+            return Response.response(Response.SUCCESS,
+                    "Cliente deletado com sucesso");
+        }
+
+        return Response.response(Response.ERROR,
+                "Não foi possível deletar o cliente");
+    }
+}
---
+++ app/src/main/java/unipar/invictus/app/controller/LoginController.java
@@ -0,0 +1,54 @@
+package unipar.invictus.app.controller;
+
+import unipar.invictus.app.helpers.Response;
+import unipar.invictus.app.entity.Usuario;
+import unipar.invictus.app.repository.UsuarioRepository;
+
+public class LoginController {
+    private final UsuarioRepository usuarioRepository;
+
+    public LoginController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Response cadastrarUsuario(String nome, String email, String senha) {
+        Usuario savedUser = usuarioRepository.getByEmail(email);
+
+        if (savedUser.getEmail().equals(email)) {
+            return Response.response(Response.SUCCESS,
+                    "Já existe um usuário cadastrado com este e-mail");
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+
+        long userRegistered = usuarioRepository.insert(usuario);
+
+        if (userRegistered > 0) {
+            return Response.response(Response.SUCCESS,
+                    "Usuário cadastrado com sucesso");
+        }
+
+        return Response.response(Response.ERROR,
+                "Não foi possível cadastrar o usuário");
+    }
+
+    public Response autenticarUsuario(String email, String senha) {
+        Usuario usuario = usuarioRepository.getByEmail(email);
+
+        if (usuario == null) {
+            return Response.response(Response.ERROR,
+                    "Este email não está cadastrado.");
+        }
+
+        if (!usuario.getSenha().equals(senha)) {
+            return Response.response(Response.ERROR,
+                    "Senha incorreta.");
+        }
+
+        return Response.response(Response.SUCCESS,
+                "Usuário autenticado com sucesso.");
+    }
+}
