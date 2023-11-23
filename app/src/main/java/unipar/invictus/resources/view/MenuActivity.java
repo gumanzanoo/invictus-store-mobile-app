fa9d7a432be64ed8fc0bbcb792f7c675f4e89fac
gumanzanoo <gustavomanzanoluiz@hotmail.com>
2023-11-23 00:17:07 -0300

    xore

--- app/src/main/java/unipar/invictus/resources/view/MenuActivity.java
+++ app/src/main/java/unipar/invictus/resources/view/MenuActivity.java
@@ -1,42 +1,26 @@
 package unipar.invictus.resources.view;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
 
 import unipar.invictus.R;
-import unipar.invictus.app.controller.VendaController;
-import unipar.invictus.app.entity.Venda;
 import unipar.invictus.app.helpers.Activity;
-import unipar.invictus.resources.adapter.VendaListAdapter;
 
 public class MenuActivity extends AppCompatActivity {
-
-    private Button btHome;
-    private Button btCatalogo;
-    private Button btExtrato;
-    private Button btUsuario;
-    private Button btProduto;
-    private Button btCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        btHome = findViewById(R.id.btHome);
-        btCatalogo = findViewById(R.id.btCatalogo);
-        btExtrato = findViewById(R.id.btExtrato);
-        btUsuario = findViewById(R.id.btUsuario);
-        btProduto = findViewById(R.id.btProduto);
-        btCliente = findViewById(R.id.btCliente);
+        Button btHome = findViewById(R.id.btHome);
+        Button btCatalogo = findViewById(R.id.btCatalogo);
+        Button btExtrato = findViewById(R.id.btExtrato);
+        Button btUsuario = findViewById(R.id.btUsuario);
+        Button btProduto = findViewById(R.id.btProduto);
+        Button btCliente = findViewById(R.id.btCliente);
 
         btHome.setOnClickListener(view -> Activity.run(this, HomeActivity.class));
         btCatalogo.setOnClickListener(view -> Activity.run(this, CatalogoActivity.class));
