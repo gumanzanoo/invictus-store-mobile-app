f772bb53fb50a2cf77e56d7c8ef1488c24df83cb
Cristyan Morais <cristyan.morais123@gmail.com>
2023-11-24 20:03:50 -0300

    chore

--- app/src/main/java/unipar/invictus/resources/view/DetalhesProdutoActivity.java
+++ app/src/main/java/unipar/invictus/resources/view/DetalhesProdutoActivity.java
@@ -1,13 +1,18 @@
 package unipar.invictus.resources.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import unipar.invictus.R;
+import unipar.invictus.app.controller.ProdutoController;
+import unipar.invictus.app.entity.Produto;
+import unipar.invictus.app.helpers.Response;
 import unipar.invictus.resources.helpers.Activity;
 
 public class DetalhesProdutoActivity extends AppCompatActivity {
@@ -16,11 +21,29 @@ public class DetalhesProdutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produto);
 
+        int produtoId = getIntent().getIntExtra("id", 1);
+
         ImageButton menuButton = findViewById(R.id.menuButton);
         FloatingActionButton btEditar = findViewById(R.id.btEditar);
         FloatingActionButton btDeletar = findViewById(R.id.btDeletar);
 
         menuButton.setOnClickListener(view -> Activity.run(this, MenuActivity.class));
-        btEditar.setOnClickListener(view -> Activity.run(this, EditarProdutoActivity.class));
+        btEditar.setOnClickListener(view -> editarProduto(produtoId));
+        btDeletar.setOnClickListener(view -> deletarProduto(produtoId));
+    }
+
+    private void deletarProduto(int produtoId) {
+        ProdutoController produtoController = new ProdutoController(this);
+        Response response = produtoController.delete(produtoId);
+        Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void editarProduto(int produtoId) {
+        ProdutoController produtoController = new ProdutoController(this);
+        Produto produto = produtoController.getById(produtoId);
+
+        Intent intent = new Intent(this, EditarProdutoActivity.class);
+        intent.putExtra("id", produto.getId());
+        this.startActivity(intent);
     }
 }
--- app/src/main/java/unipar/invictus/resources/view/EditarProdutoActivity.java
+++ app/src/main/java/unipar/invictus/resources/view/EditarProdutoActivity.java
@@ -1,13 +1,17 @@
 package unipar.invictus.resources.view;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import unipar.invictus.R;
+import unipar.invictus.app.controller.ProdutoController;
+import unipar.invictus.app.entity.Produto;
 import unipar.invictus.resources.helpers.Activity;
 
 public class EditarProdutoActivity extends AppCompatActivity {
@@ -16,8 +20,14 @@ public class EditarProdutoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_produto);
 
+        TextView edCodigo = findViewById()
+
         ImageButton menuButton = findViewById(R.id.menuButton);
         FloatingActionButton btSalvar = findViewById(R.id.btSalvar);
+
+        int produtoId = getIntent().getIntExtra("id", 1);
+        ProdutoController produtoController = new ProdutoController(this);
+        Produto produto = produtoController.getById(produtoId);
 
         menuButton.setOnClickListener(view -> Activity.run(this, MenuActivity.class));
     }
